6c0d7a2eeb5e818d0942a922cd13d575b269ea40
novakw267 <novakw267@gmail.com>
2017-03-22 10:51:49 -0400

    Refactor lists route
    Display only list titles using shopping-list/card component
    Create a list model
    Refactor lists model hook to get data from the API

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -2,22 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model () {
-    return [
-      {
-        title: 'Favorites',
-        items: [
-          { content: 'Cats' },
-          { content: 'Dogs' },
-          { content: 'Scotch' },
-        ],
-      }, {
-        title: 'Todos',
-        items: [
-          { content: 'Learn Ember' },
-          { content: 'Change Oil' },
-          { content: 'Visit Portland' },
-        ],
-      },
-    ];
+    console.log();
+    return this.get('store').findAll('list');
   },
 });
