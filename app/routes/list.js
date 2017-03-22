2d70cd87deb624dd47befb4cbfbc906c625ebcc2
novakw267 <novakw267@gmail.com>
2017-03-22 12:22:07 -0400

    Persist changes in item to the database
    Change listItemCompleted to be an alias for item.done
    Refactor the toggleDone action to send the action up with the item
    Send the toggleDone Action up to the route (as toggleItemDone)
    Persist changes to the API in the route toggleItemDone action.
    Added Back button to the Lists.hbs

--- app/routes/list.js
+++ app/routes/list.js
@@ -4,4 +4,10 @@ export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('list', params.list_id);
   },
+  actions: {
+    toggleItemDone(item) {
+      item.toggleProperty('done');
+      item.save();
+    }
+  }
 });
