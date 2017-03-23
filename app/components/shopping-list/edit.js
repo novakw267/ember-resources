2e45419e69282a534b6450979c49ad5a79a628b7
novakw267 <novakw267@gmail.com>
2017-03-23 11:15:12 -0400

    Implement create list functionality
    Create lists/index route
    Move content from lists route to lists/index route
    Add outlet to lists route so nested content displays
    Create lists/new route
    Use the shopping-list/edit component to create a new list
    Add createList action to lists route, and bind it to the save
    Action on the edit component (in lists/new)

--- app/components/shopping-list/edit.js
+++ app/components/shopping-list/edit.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     save () {
-      console.log("Don't forget to save your game!");
       this.sendAction('save', this.get('list'));
     }
   }
---
+++ app/routes/lists/new.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model () {
+    return this.get('store').createRecord('list', {});
+  },
+  actions: {
+    createList(list) {
+      list.save()
+      .then(() => this.transitionTo('lists'));
+    }
+  }
+});
